acb1df0fdb52ad57c2941e80e89029bf47194a3f
josh-weiss76 <59374753+josh-weiss76@users.noreply.github.com>
2020-06-16 11:30:11 -0400

    added divide method and try/catch for division by zero

--- csharp-web-dev-lsn9exceptions/Program.cs
+++ csharp-web-dev-lsn9exceptions/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq.Expressions;
 
 namespace csharp_web_dev_lsn9exceptions
 {
@@ -8,18 +9,39 @@ namespace csharp_web_dev_lsn9exceptions
         static double Divide(double x, double y)
         {
             // Write your code here!
+            try
+            {
+                if (y != 0)
+                {
+                    double result = x / y;
+                    return result;
+                }
+                throw new ArgumentOutOfRangeException("Cannot divide by zero");
+            }
+            catch(ArgumentOutOfRangeException)
+            {
+                Console.WriteLine("Cannot divide by zero, check your input and rerun program");
+                double fail = 0;
+                return fail;
+            }
+            
+
         }
 
         static int CheckFileExtension(string fileName)
         {
-            // Write your code here!
+            int result = 0;
+            return result;// Write your code here!
         }
 
 
         static void Main(string[] args)
         {
             // Test out your Divide() function here!
-
+            double test1 = Divide(8, 2);
+            Console.WriteLine(test1);
+            double test2 = Divide(8,0);
+            Console.WriteLine(test2);
             // Test out your CheckFileExtension() function here!
             Dictionary<string, string> students = new Dictionary<string, string>();
             students.Add("Carl", "Program.cs");
