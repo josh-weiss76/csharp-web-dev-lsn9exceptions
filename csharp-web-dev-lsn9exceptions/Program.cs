f023bf9b2b3bd32e50746b325b9d70cfaf19074a
josh-weiss76 <59374753+josh-weiss76@users.noreply.github.com>
2020-06-16 15:54:38 -0400

    took try/catch out of methods and put them in the program main

--- csharp-web-dev-lsn9exceptions/Program.cs
+++ csharp-web-dev-lsn9exceptions/Program.cs
@@ -8,49 +8,29 @@ namespace csharp_web_dev_lsn9exceptions
     {
         static double Divide(double x, double y)
         {
-            try
+            if (y != 0)
             {
-                if (y != 0)
-                {
-                    double result = x / y;
-                    return result;
-                }
-                throw new ArgumentOutOfRangeException("Cannot divide by zero");
+                 double result = x / y;
+                 return result;
             }
-            catch(ArgumentOutOfRangeException)
-            {
-                Console.WriteLine("Cannot divide by zero, check your input and rerun program");
-                double fail = 0;
-                return fail;
-            }
-            
-
+            throw new ArgumentOutOfRangeException("y","Cannot divide by zero");
         }
 
         static int CheckFileExtension(string fileName)
         {
-            try
+            if (fileName.Contains(".cs"))
             {
-                if (fileName.Contains(".cs"))
-                {
-                    int pointValue = 1;
-                    return pointValue;
-                }
-                else if (fileName != "")
-                {
-                    int pointValue = 0;
-                    return pointValue;
-                }
-                else 
-                {
-                    throw new ArgumentNullException("No student submission");
-                }
+                int pointValue = 1;
+                return pointValue;
             }
-            catch(ArgumentNullException)
+            else if (fileName != "")
             {
-                Console.WriteLine("The student does not have a file submission in the system, check with student on status of project.");
                 int pointValue = 0;
                 return pointValue;
+            }
+            else 
+            {
+                throw new ArgumentNullException("fileName","No student submission");
             }
         }
 
@@ -58,33 +38,38 @@ namespace csharp_web_dev_lsn9exceptions
         static void Main(string[] args)
         {
             // Test out your Divide() function here!
-            double test1 = Divide(8, 2);
-            Console.WriteLine(test1);
-            double test2 = Divide(8,0);
-            Console.WriteLine(test2);
+            double x = 8;
+            double y = 0;
+            double z = 4;
+            Console.WriteLine(Divide(x, z));
+            try
+            {
+                Divide(x, y);
+            }
+            catch (ArgumentOutOfRangeException e)
+            {
+                Console.WriteLine(e.Message);
+            }
             // Test out your CheckFileExtension() function here!
             Dictionary<string, string> students = new Dictionary<string, string>();
             students.Add("Carl", "Program.cs");
             students.Add("Brad", "");
-            students.Add("Elizabeth", "MyCode.cs");
+            students.Add("Elizabeth", "MyCode");
             students.Add("Stefanie", "CoolProgram.cs");
 
 
-            List<string> name = new List<string>();
-            List<string> submission = new List<string>();
-            foreach (string student in students.Keys)
-            {
-                name.Add(student);
-            }
-            foreach (string project in students.Values)
-            {
-                submission.Add(project);
-            }
-            for (int j = 0; j < name.Count; j++)
-            {
-                Console.WriteLine(name[j] + " : " + CheckFileExtension(submission[j]));
-            }
+            foreach (KeyValuePair<string, string> student in students)
+            { 
+                try
+                {
+                    Console.WriteLine(student.Key + " : " + CheckFileExtension(student.Value));
+                }
+                catch(ArgumentNullException)
+                { 
+                    Console.WriteLine(student.Key + " does not have a file submission in the system, check with student on status of project.");
+                }
 
+            }
         }
     }
 }
